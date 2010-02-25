1c124c79115babc410bb9c5e3f3ba1c7c73b25ad
Jake <pearsonjj@yahoo.com>
2010-02-25 15:16:44 -0700

    Default Add

---
+++ SternCatcher/Enclosure.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using Microsoft.ServiceModel.Samples.Syndication;
+using System.Xml.Serialization;
+
+namespace SternCatcher
+{
+	[DataContract(Name = FeedConstants.Category, Namespace = FeedConstants.FeedNamespace)]
+	[XmlType(TypeName = FeedConstants.Category, Namespace = FeedConstants.FeedNamespace)]
+	public class Enclosure
+	{
+		public Enclosure(string domain, string name)
+		{
+			CategoryDomain = domain;
+			CategoryValue = name;
+		}
+
+		[DataMember(Name = FeedConstants.CategoryDomain)]
+		[XmlElement(ElementName = FeedConstants.CategoryDomain)]
+		public string CategoryDomain
+		{
+			get;
+			set;
+		}
+
+		[DataMember(Name = FeedConstants.CategoryValue)]
+		[XmlElement(ElementName = FeedConstants.CategoryValue)]
+		public string CategoryValue
+		{
+			get;
+			set;
+		}
+	}
+}
---
+++ SternCatcher/MainForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace SternCatcher
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// notifyIcon
+			// 
+			this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+			this.notifyIcon.Text = "Stern Catcher 1.0";
+			this.notifyIcon.Visible = true;
+			this.notifyIcon.DoubleClick += new System.EventHandler(this.notifyIcon_DoubleClick);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(164, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Connected: Feel Free to Minimize";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(405, 83);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.Text = "Stern Catcher";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+			this.Resize += new System.EventHandler(this.MainForm_Resize);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.NotifyIcon notifyIcon;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
