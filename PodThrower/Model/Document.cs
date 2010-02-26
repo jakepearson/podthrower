45c6ac543684fe73677c1ffb30eccd60317f7096
Jake <pearsonjj@yahoo.com>
2010-02-26 14:07:02 -0700

    Implemented Notify Icon

--- PodThrower/Model/Document.cs
+++ PodThrower/Model/Document.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.ComponentModel;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
+using PodThrower.View;
 
 namespace PodThrower.Model
 {
@@ -11,7 +13,9 @@ namespace PodThrower.Model
 	{
 		#region Member Variables
 		int port;
+		bool connected;
 		ObservableCollection<Feed> feeds;
+		ICommand editCommand;
 		#endregion
 
 		#region Properties
@@ -28,10 +32,40 @@ namespace PodThrower.Model
 			}
 		}
 
+		public bool Connected
+		{
+			get { return connected; }
+			set
+			{
+				if (connected != value)
+				{
+					connected = value;
+					LaunchChanged("Connected");
+					LaunchChanged("Message");
+				}
+			}
+		}
+
+		public string Message
+		{
+			get { return "Pod Thrower: " + (Connected ? "Connected" : "Not Connected"); }
+		}
+
 		public ObservableCollection<Feed> Feeds
 		{
 			get { return feeds ?? (feeds = new ObservableCollection<Feed>()); }
 		}
 		#endregion
+
+		#region Commands
+		public ICommand EditCommand
+		{
+			get { return editCommand ?? (editCommand = new RelayCommand(p => ShowEditor())); }
+		}
+		#endregion
+
+		void ShowEditor()
+		{
+		}
 	}
 }
