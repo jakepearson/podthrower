33895a34a3b3fdbeb96bdfffc0149dbacfc6e99a
Jake <pearsonjj@yahoo.com>
2010-02-26 10:59:35 -0700

    Started WPF Implementation

---
+++ PodThrower/Model/Feed.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PodThrower.Model
+{
+	public class Feed : BaseModel
+	{
+		#region Member Variables
+		string folder;
+		string title;
+		#endregion
+
+		#region Properties
+		public string Folder
+		{
+			get { return folder; }
+			set
+			{
+				if (folder != value)
+				{
+					folder = value;
+					LaunchChanged("Folder");
+				}
+			}
+		}
+
+		public string Title
+		{
+			get { return title; }
+			set
+			{
+				if (title != value)
+				{
+					title = value;
+					LaunchChanged("Title");
+				}
+			}
+		}
+		#endregion
+	}
+}
