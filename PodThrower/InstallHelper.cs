9082c875f5e8dc0952713b4d17a91b7a03f360e1
Jake Pearson <325306+jakepearson@users.noreply.github.com>
2010-02-27 13:37:06 -0700

    Added Setup Project

---
+++ PodThrower/InstallHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Diagnostics;
+
+namespace PodThrower
+{
+	[RunInstaller(true)]
+	public partial class InstallHelper : Installer
+	{
+		public InstallHelper()
+		{
+			InitializeComponent();
+		}
+
+		public override void Install(IDictionary stateSaver)
+		{
+			base.Install(stateSaver);
+
+			Process.Start("netsh", @"http add urlacl url=http://+:4242/PodThrower user=BUILTIN\Users");
+		}
+	}
+}
