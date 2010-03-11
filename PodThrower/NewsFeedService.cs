720dcaad074904722782d5989028e1a933051481
Jake <pearsonjj@yahoo.com>
2010-03-11 10:13:31 -0700

    Attempted to lighten memory consumption

--- PodThrower/NewsFeedService.cs
+++ PodThrower/NewsFeedService.cs
@@ -36,6 +36,7 @@ namespace PodThrower
 		Stream GetImage(string id);
 	}
 
+	[ServiceBehavior(InstanceContextMode=InstanceContextMode.Single)]
 	public class NewsFeedService : INewsFeed
 	{
 		#region Properties
